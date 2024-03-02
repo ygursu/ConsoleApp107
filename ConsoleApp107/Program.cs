ccbb7bc101f87268bc5cf5a67b55554923834220
ygursu <yasemingrs@gmail.com>
2024-03-02 22:36:56 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp107/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string ad,sifre;
+Console.Write("Kullanıcı adını giriniz:");
+ad=Console.ReadLine();
+Console.Write("Şifre giriniz:");
+sifre=Console.ReadLine();
+if(ad=="admin" && sifre=="12345")
+{
+  Console.WriteLine("Giriş başarılı");
+}
+else
+{
+  Console.WriteLine("Kullanıcı adını veya şifreyi kontrol ediniz");
+}
+Console.ReadKey();
